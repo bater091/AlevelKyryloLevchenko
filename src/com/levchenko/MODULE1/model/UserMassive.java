98e19b5f41021caa8877e82e9c2db9623ac0ba4a
bater091 <tggh935@gmail.com>
2021-11-13 12:51:03 +0200

    add module

---
+++ src/com/levchenko/MODULE1/model/UserMassive.java
@@ -0,0 +1,13 @@
+package com.levchenko.MODULE1.model;
+
+public class UserMassive {
+    private int[] userArgs;
+
+    public int[] getUserArgs() {
+        return userArgs;
+    }
+
+    public void setUserArgs(int[] userArgs) {
+        this.userArgs = userArgs;
+    }
+}
